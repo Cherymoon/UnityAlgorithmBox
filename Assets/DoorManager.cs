88b268ab8cae8241ebb7217102fdccc457e003a5
Guilherme Frankenberger <nemseialistar@gmail.com>
2023-12-21 21:26:42 -0300

    add bit mask

--- Assets/DoorManager.cs
+++ Assets/DoorManager.cs
@@ -6,6 +6,19 @@ public class DoorManager : MonoBehaviour
 {
     int doorType = AttributeManager.MAGIC;
 
+    void OnCollisionEnter(Collision other)
+    {
+        if ((other.gameObject.GetComponent<AttributeManager>().attributes & doorType) != 0)
+        {
+            this.GetComponent<BoxCollider>().isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.GetComponent<BoxCollider>().isTrigger = false;
+    }
+
     void Start()
     {
 
