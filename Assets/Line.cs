ec3cc858d31c7913f70eb67c95fd62d952fef683
Guilherme Frankenberger <nemseialistar@gmail.com>
2024-01-03 20:21:27 -0300

    add line/plane intersection schemes

--- Assets/Line.cs
+++ Assets/Line.cs
@@ -1,40 +1,71 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Line
 {
-    public Vector3 A;
-    public Vector3 B;
-    public Vector3 v;
+    Coords A, B, v;
 
-    public enum LINETYPE { LINE, SEGMENT, RAY };
-    LINETYPE type;
-
-    public Line(Vector3 A, Vector3 B, LINETYPE type)
+    public enum LINETYPE
     {
-        this.A = A;
-        this.B = B;
-        this.v = B - A;
-        this.type = type;
+        LINE,
+        SEGMENT,
+        RAY
     }
 
-    public Vector3 Lerp(float t)
+    LINETYPE type;
+
+    public Line(Coords _A, Coords _B, LINETYPE _type)
     {
-        if (type == LINETYPE.LINE)
+        A = _A;
+        B = _B;
+        type = _type;
+        v = new Coords(B.x - A.x, B.y - A.y, B.z - A.z);
+    }
+
+    public Line(Coords _A, Coords _V)
+    {
+        A = _A;
+        v = _V;
+        B = _A + v;
+        type = LINETYPE.SEGMENT;
+    }
+
+
+    public float IntersectsAt(Line l)
+    {
+        if(HolisticMath.Dot(Coords.Perp(l.v), v) == 0)
         {
-            t = Mathf.Clamp(t, float.MinValue, float.MaxValue);
-        }
-        else if (type == LINETYPE.SEGMENT)
-        {
-            t = Mathf.Clamp01(t);
-        }
-        else if (type == LINETYPE.RAY)
-        {
-            t = Mathf.Clamp(t, 0, float.MaxValue);
+            return float.NaN;
         }
 
-        return A + (v * t);
+        Coords c = l.A - this.A;
+        float t = HolisticMath.Dot(Coords.Perp(l.v), c) / HolisticMath.Dot(Coords.Perp(l.v), v);
+
+        if(t < 0 || t > 1 && type == LINETYPE.SEGMENT)
+        {
+            return float.NaN;
+        }
+
+        return t;
+    }
+
+    public void Draw(float width, Color col)
+    {
+        Coords.DrawLine(A, B, width, col);
+    }
+
+    public Coords Lerp(float t)
+    {
+        if (type == LINETYPE.SEGMENT)
+            t = Mathf.Clamp(t, 0, 1);
+        else if (type == LINETYPE.RAY && t < 0)
+            t = 0;
+
+        float xt = A.x + v.x * t;
+        float yt = A.y + v.y * t;
+        float zt = A.z + v.z * t;
+
+        return new Coords(xt, yt, zt);
     }
 }
